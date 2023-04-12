321ae4be84bb2c92605eae7dacaba957b7dd325c
wewelski <f.roowell@gmail.com>
2023-04-12 16:03:30 +0800

    replace form with simple message box

---
+++ script.js
@@ -0,0 +1,12 @@
+const post = document.getElementById('post');
+
+post.addEventListener('click',function() {
+    const commentBoxValue = document.getElementById('comment-box').value;
+
+    const li = document.createElement('li');
+    const text = document.createTextNode(commentBoxValue);
+
+    li.appendChild(text);
+    document.getElementById('unordered').appendChild(li);
+
+});
